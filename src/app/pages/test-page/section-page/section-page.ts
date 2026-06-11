5398f82b6bd339211ecd44a5a96ebed3f9a48cb6
Bryan Williams <bwillwork@proton.me>
2026-06-11 12:47:49 -0400

    Adding section page

---
+++ src/app/pages/test-page/section-page/section-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-page',
+  imports: [],
+  templateUrl: './section-page.html',
+  styleUrl: './section-page.css',
+})
+export class SectionPage {}
