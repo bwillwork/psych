01c32195e2e882713749d624270fa1fb3b8d84a6
Bryan Williams <bwillwork@proton.me>
2026-06-11 13:36:31 -0400

    Adding question types and a test service interface

---
+++ src/app/types/test.types.ts
@@ -0,0 +1,33 @@
+export type FiveScaleChoice = 1 | 2 | 3 | 4 | 5;
+
+export type TwoMultipleChoiceKeys = 'A' | 'B';
+export type FourMultipleChoiceKeys = TwoMultipleChoiceKeys | 'C' | 'D';
+export type FiveMultipleChoiceKeys = FourMultipleChoiceKeys | 'E';
+
+export interface Question {
+  question: string
+}
+export interface TrueFalseQuestion extends Question {
+  response: boolean | undefined
+}
+export interface FiveScaleQuestion extends Question {
+  response: FiveScaleChoice| undefined
+}
+export interface TwoMultipleChoiceQuestion extends Question {
+  responses: {[key in TwoMultipleChoiceKeys]: string},
+  response: TwoMultipleChoiceKeys| undefined
+}
+export interface FourMultipleChoiceQuestion extends Question {
+  responses: {[key in FourMultipleChoiceKeys]: string},
+  response: FourMultipleChoiceKeys| undefined
+}
+export interface FiveMultipleChoiceQuestion extends Question {
+  responses: {[key in FiveMultipleChoiceKeys]: string},
+  response: FiveMultipleChoiceKeys| undefined
+}
+
+
+export interface TestService<Q> {
+  getQuestions(): Array<Q>;
+  canSeeResult(): boolean;
+}
