c15a83055ca823690c9ac3b05acd67fae20c8f9b
Bryan Williams <bwillwork@proton.me>
2026-06-19 15:38:28 -0400

    Adding code for questions

--- src/app/components/forms/questions/five-multiple-choice-question-elm/five-multiple-choice-question-elm.ts
+++ src/app/components/forms/questions/five-multiple-choice-question-elm/five-multiple-choice-question-elm.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { FiveMultipleChoiceKeys, FiveMultipleChoiceQuestion } from '../../../../types/test.types';
 
 @Component({
   selector: 'app-five-multiple-choice-question-elm',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './five-multiple-choice-question-elm.html',
   styleUrl: './five-multiple-choice-question-elm.css',
 })
-export class FiveMultipleChoiceQuestionElm {}
+export class FiveMultipleChoiceQuestionElm {
+
+  question = input.required<FiveMultipleChoiceQuestion>();
+  responseKeys = computed(() => {
+    const q = this.question();
+    const keys = Object.keys(q.responses) as Array<FiveMultipleChoiceKeys>;
+    return keys.sort();
+  });
+
+}
