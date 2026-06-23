0632c0a64572766f3501325123c4f7e7b0011a06
Bryan Williams <bwillwork@proton.me>
2026-06-22 20:54:08 -0400

    Adding code for question elements

--- src/app/components/forms/questions/four-multiple-choice-question-elm/four-multiple-choice-question-elm.ts
+++ src/app/components/forms/questions/four-multiple-choice-question-elm/four-multiple-choice-question-elm.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, computed, input} from '@angular/core';
+import {FourMultipleChoiceKeys, FourMultipleChoiceQuestion} from '../../../../types/test.types';
 
 @Component({
   selector: 'app-four-multiple-choice-question-elm',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './four-multiple-choice-question-elm.html',
   styleUrl: './four-multiple-choice-question-elm.css',
 })
-export class FourMultipleChoiceQuestionElm {}
+export class FourMultipleChoiceQuestionElm {
+  question = input.required<FourMultipleChoiceQuestion>();
+  responseKeys = computed(() => {
+    const q = this.question();
+    const keys = Object.keys(q.responses) as Array<FourMultipleChoiceKeys>;
+    return keys.sort();
+  });
+
+
+}
--- src/app/components/forms/questions/two-multiple-choice-question-elm/two-multiple-choice-question-elm.ts
+++ src/app/components/forms/questions/two-multiple-choice-question-elm/two-multiple-choice-question-elm.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, computed, input} from '@angular/core';
+import {TwoMultipleChoiceKeys, TwoMultipleChoiceQuestion} from '../../../../types/test.types';
 
 @Component({
   selector: 'app-two-multiple-choice-question-elm',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './two-multiple-choice-question-elm.html',
   styleUrl: './two-multiple-choice-question-elm.css',
 })
-export class TwoMultipleChoiceQuestionElm {}
+export class TwoMultipleChoiceQuestionElm {
+
+  question = input.required<TwoMultipleChoiceQuestion>();
+  responseKeys = computed(() => {
+    const q = this.question();
+    const keys = Object.keys(q.responses) as Array<TwoMultipleChoiceKeys>;
+    return keys.sort();
+  });
+
+}
