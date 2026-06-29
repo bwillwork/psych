34bc15da2bb89f9a958c41f6393d13afa3a07724
Bryan Williams <bwillwork@proton.me>
2026-06-29 19:52:46 -0400

    Adding code for evaluators

--- src/app/data/selectors/results.selectors.ts
+++ src/app/data/selectors/results.selectors.ts
@@ -1,6 +1,19 @@
 import { createSelector } from '@ngrx/store';
-import {AllQuestionTypes, AppState} from '../../types/test.types';
+import {
+  AllQuestionTypes,
+  AppState,
+  BigFiveResult,
+  FiveModernRelationshipsResult,
+  MyersBrigsResult,
+  PenTestResult, SeductionArchetypesResult, SixStylesOfLovingResult
+} from '../../types/test.types';
 import {selectIsFinished, selectTestChoices} from './test.selectors';
+import {
+  evaluateBigFiveTest,
+  evaluateFiveModernTest,
+  evaluateMyersBrigsTest,
+  evaluatePenTest, evaluateSeductionTest, evaluateSixStylesOfLovingTest
+} from '../../util/results.util';
 
 export const selectBigFiveTestData = (state: AppState) => state.bigFive;
 export const selectMyersBrigsTestData = (state: AppState) => state.myersBrigs;
@@ -10,15 +23,88 @@ export const selectFiveModernRelTestData = (state: AppState) => state.fiveModern
 export const selectSeductionTestData = (state: AppState) => state.seduction;
 export const selectSixStylesTestData = (state: AppState) => state.sixStyles;
 
+
+// Personality
 export const selectBigFiveResult = createSelector(
   selectBigFiveTestData,
   selectTestChoices,
   selectIsFinished,
-  (questions,choices,isFinished) => {
+  (questions,choices,isFinished): BigFiveResult => {
     if(isFinished && choices.bigFive) {
-
+      return evaluateBigFiveTest(questions);
     } else {
-      return
+      return {agreeableness: 0, conscientiousness: 0, extroversion: 0, neuroticism: 0, openness: 0};
     }
   }
-)
+);
+
+export const selectMyersBrigsResult = createSelector(
+  selectMyersBrigsTestData,
+  selectTestChoices,
+  selectIsFinished,
+  (questions,choices,isFinished): MyersBrigsResult => {
+    if(isFinished && choices.bigFive) {
+      return evaluateMyersBrigsTest(questions);
+    } else {
+      return {
+        decisions: {feeling: 0, thinking: 0},
+        energy: {extroversion: 0, introversion: 0},
+        information: {intuition: 0, sensing: 0},
+        lifestyle: {judging: 0, perceiving: 0}
+      };
+    }
+  }
+);
+
+export const selectPenResult = createSelector(
+  selectPenTestData,
+  selectTestChoices,
+  selectIsFinished,
+  (questions,choices,isFinished): PenTestResult => {
+    if(isFinished && choices.bigFive) {
+      return evaluatePenTest(questions);
+    } else {
+      return {extroversion: 0, neuroticism: 0, psychoticism: 0};
+    }
+  }
+);
+
+// Romantic
+export const selectFiveModernRelResult = createSelector(
+  selectFiveModernRelTestData,
+  selectTestChoices,
+  selectIsFinished,
+  (questions,choices,isFinished): FiveModernRelationshipsResult => {
+    if(isFinished && choices.bigFive) {
+      return evaluateFiveModernTest(questions);
+    } else {
+      return {A: 0, B: 0, C: 0, D: 0, E: 0};
+    }
+  }
+);
+
+export const selectSeductionResult = createSelector(
+  selectSeductionTestData,
+  selectTestChoices,
+  selectIsFinished,
+  (questions,choices,isFinished): SeductionArchetypesResult => {
+    if(isFinished && choices.bigFive) {
+      return evaluateSeductionTest(questions);
+    } else {
+      return {A: 0, B: 0, C: 0, D: 0};
+    }
+  }
+);
+
+export const selectSixStylesResult = createSelector(
+  selectSixStylesTestData,
+  selectTestChoices,
+  selectIsFinished,
+  (questions,choices,isFinished): SixStylesOfLovingResult => {
+    if(isFinished && choices.bigFive) {
+      return evaluateSixStylesOfLovingTest(questions);
+    } else {
+      return {agape: 0, eros: 0, ludus: 0, mania: 0, pragma: 0, storage: 0};
+    }
+  }
+);
--- src/app/util/results.util.ts
+++ src/app/util/results.util.ts
@@ -1,17 +1,23 @@
 import {
-  BigFiveResult, FiveModernRelationshipsResult,
+  BigFiveResult, FiveModernRelationshipsResult, FiveMultipleChoiceAnswer,
   FiveMultipleChoiceQuestion, FiveScaleAnswer,
-  FiveScaleQuestion, FourMultipleChoiceQuestion, MyersBrigsResult, PenTestResult, SeductionArchetypesResult,
+  FiveScaleQuestion,
+  FourMultipleChoiceAnswer, FourMultipleChoiceQuestion, MyersBrigsResult, PenTestResult, SeductionArchetypesResult,
   SixStylesOfLovingResult,
-  TrueFalseQuestion,
+  TrueFalseQuestion, TwoMultipleChoiceAnswer, TwoMultipleChoiceKeys,
   TwoMultipleChoiceQuestion
 } from '../types/test.types';
+
+
+function _in<T>(array: Array<T>, elm: T) {
+  return array.findIndex(e => e === elm) !== -1;
+}
 
 // Personality Tests
 export function evaluateBigFiveTest(questions: Array<FiveScaleQuestion>): BigFiveResult {
 
   function filter(questions: Array<FiveScaleQuestion>, questionIdNums: Array<number>) {
-    return questions.filter(q => q.id.num in questionIdNums);
+    return questions.filter(q => _in(questionIdNums,q.id.num));
   }
 
   function addScores(questions: Array<FiveScaleQuestion>) {
@@ -47,27 +53,94 @@ export function evaluateBigFiveTest(questions: Array<FiveScaleQuestion>): BigFiv
 
 
 export function evaluateMyersBrigsTest(questions: Array<TwoMultipleChoiceQuestion>): MyersBrigsResult {
+  function filter(questions: Array<TwoMultipleChoiceQuestion>,idNums:Array<number>, choice: TwoMultipleChoiceAnswer) {
+    return questions.filter(q => _in(idNums,q.id.num) && q.answer === choice);
+  }
+
+  function addUpAs(questions: Array<TwoMultipleChoiceQuestion>,idNums:Array<number>) {
+    return filter(questions,idNums,'A').length;
+  }
+
+  function addUpBs(questions: Array<TwoMultipleChoiceQuestion>,idNums:Array<number>) {
+    return filter(questions,idNums,'B').length;
+  }
+
   return {
-    decisions: {feeling: 0, thinking: 0},
-    energy: {extroversion: 0, introversion: 0},
-    information: {intuition: 0, sensing: 0},
-    lifestyle: {judging: 0, perceiving: 0}
+    decisions: {feeling: addUpBs(questions,[11,12,13,14,15]), thinking: addUpAs(questions,[11,12,13,14,15])},
+    energy: {extroversion: addUpAs(questions,[1,2,3,4,5]), introversion: addUpBs(questions,[1,2,3,4,5])},
+    information: {intuition: addUpBs(questions,[6,7,8,9,10]), sensing: addUpAs(questions,[6,7,8,9,10])},
+    lifestyle: {judging: addUpAs(questions,[16,17,18,19,20]), perceiving: addUpBs(questions,[16,17,18,19,20])}
   };
 }
 
 export function evaluatePenTest(questions: Array<TrueFalseQuestion>): PenTestResult {
-  return {extroversion: 0, neuroticism: 0, psychoticism: 0};
+  function addUp(questions: Array<TrueFalseQuestion>, idNums: Array<number>, expectedAnswers: Array<boolean>) {
+    let result = 0;
+    for(const q in questions) {
+      const question = questions[q];
+      const idIndex = idNums.findIndex(idNum => idNum === question.id.num);
+      const expectedAnswer = expectedAnswers[idIndex];
+      if(question.answer === expectedAnswer) result += 1;
+    }
+    return result;
+  }
+
+
+  return {
+    extroversion: addUp(questions,[1,4,7,10,13],[true,true,true,true,false]),
+    neuroticism: addUp(questions,[2,5,8,11,14],[true,true,true,true,true]),
+    psychoticism: addUp(questions,[3,6,9,12,15],[true,false,true,false,true]),
+  };
 }
 
 // Romantic
 export function evaluateFiveModernTest(questions: Array<FiveMultipleChoiceQuestion>): FiveModernRelationshipsResult {
-  return {A: 0, B: 0, C: 0, D: 0, E: 0};
+
+  function addUp(questions: Array<FiveMultipleChoiceQuestion>, choice: FiveMultipleChoiceAnswer) {
+    return questions.reduce((agg,q) => {
+      return (q.answer === choice) ? agg + 1 : agg;
+    },0);
+  }
+
+  return {
+    A: addUp(questions,'A'),
+    B: addUp(questions,'B'),
+    C: addUp(questions,'C'),
+    D: addUp(questions,'D'),
+    E: addUp(questions,'E'),
+  };
 }
 
 export function evaluateSeductionTest(questions: Array<FourMultipleChoiceQuestion>): SeductionArchetypesResult {
-  return {A: 0, B: 0, C: 0, D: 0};
+  function addUp(questions: Array<FourMultipleChoiceQuestion>, choice: FourMultipleChoiceAnswer) {
+    return questions.reduce((agg,q) => {
+      return (q.answer === choice) ? agg + 1 : agg;
+    },0);
+  }
+
+  return {
+    A: addUp(questions,'A'),
+    B: addUp(questions,'B'),
+    C: addUp(questions,'C'),
+    D: addUp(questions,'D'),
+  };
 }
 
 export function evaluateSixStylesOfLovingTest(questions: Array<FiveScaleQuestion>): SixStylesOfLovingResult {
-  return {agape: 0, eros: 0, ludus: 0, mania: 0, pragma: 0, storage: 0};
+  function addScores(questions: Array<FiveScaleQuestion>, idNums: Array<number>) {
+    return questions
+      .filter(q => _in(idNums,q.id.num))
+      .reduce((agg,q) => {
+        return agg + (q.answer ?? 0);
+      },0);
+  }
+
+  return {
+    eros: addScores(questions,[1,7,13]),
+    ludus: addScores(questions,[2,8,14]),
+    storage: addScores(questions,[3,9,15]),
+    pragma: addScores(questions,[4,10,16]),
+    mania: addScores(questions,[5,11,17]),
+    agape: addScores(questions,[6,12,18]),
+  };
 }
