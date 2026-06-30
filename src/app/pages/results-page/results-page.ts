d6a7234f8cd67a74b70f3105799720b66c0277d5
Bryan Williams <bwillwork@proton.me>
2026-06-29 20:08:05 -0400

    Adding result elements to the results page

--- src/app/pages/results-page/results-page.ts
+++ src/app/pages/results-page/results-page.ts
@@ -1,9 +1,24 @@
 import {Component, inject} from '@angular/core';
 import {Store} from '@ngrx/store';
+import {BigFiveResultsElm} from '../../components/tests/personality/big-five-results-elm/big-five-results-elm';
+import {MyersBrigsResultsElm} from '../../components/tests/personality/myers-brigs-results-elm/myers-brigs-results-elm';
+import {PenResultsElm} from '../../components/tests/personality/pen-results-elm/pen-results-elm';
+import {
+  FiveModernRelResultsElm
+} from '../../components/tests/romantic/five-modern-rel-results-elm/five-modern-rel-results-elm';
+import {SeductionResultsElm} from '../../components/tests/romantic/seduction-results-elm/seduction-results-elm';
+import {SixStylesResultsElm} from '../../components/tests/romantic/six-styles-results-elm/six-styles-results-elm';
 
 @Component({
   selector: 'app-results-page',
-  imports: [],
+  imports: [
+    BigFiveResultsElm,
+    MyersBrigsResultsElm,
+    PenResultsElm,
+    FiveModernRelResultsElm,
+    SeductionResultsElm,
+    SixStylesResultsElm
+  ],
   templateUrl: './results-page.html',
   styleUrl: './results-page.css',
 })
